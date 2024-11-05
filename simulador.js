89b474e486d264afecc47f6817ffce5eeb51c2f7
nahuelcampo99 <153131076+nahuelcampo99@users.noreply.github.com>
2024-11-05 17:08:15 -0300

    primercommit

---
+++ simulador.js
@@ -0,0 +1,6 @@
+//elementos
+const formulario=document.getElementById(cuerpo)
+const montoInput=document.getElementById(monto)
+const interesInput=document.getElementById(interes)
+const reultado=document.getElementById(resultado)
+const cuotaMensual=document.getElementById(cuotaMensual)
